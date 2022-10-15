0d34800c1c558ce14c89a62ffdf4d6d917c26301
alostsock <hi@alostsock.dev>
2022-10-15 02:25:38 -0400

    initial commit

---
+++ data/dl.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Recipe.csv > Recipe.csv
+curl https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/RecipeLevelTable.csv > RecipeLevelTable.csv
+curl https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/Item.csv > Item.csv
+curl https://raw.githubusercontent.com/xivapi/ffxiv-datamining/master/csv/RecipeLookup.csv > RecipeLookup.csv
